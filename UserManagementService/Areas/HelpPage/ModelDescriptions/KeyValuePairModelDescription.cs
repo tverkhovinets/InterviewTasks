5285f2892c0f4c41d5c2b3fd12e09ac5789a9c53
Taras <Taras@TARAS-PC>
2018-02-19 17:25:51 +0200

    Add project files.

---
+++ UserManagementService/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace UserManagementService.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
